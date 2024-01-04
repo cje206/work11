6c2ea8c94d59a89f5bd85cd2ccdb39049a3450ce
cje206 <projec206@gmail.com>
2024-01-04 17:05:59 +0900

    study : css animation, javascript

---
+++ week2/0104/examjs.js
@@ -0,0 +1,9 @@
+let colors = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
+console.log(colors[2]);
+
+colors.push("white");
+console.log(colors);
+
+console.log(colors.indexOf("black"));
+
+console.log(colors.reverse());
