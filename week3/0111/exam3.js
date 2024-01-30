b5291d7d6cc523d67d7f2bab309fb045bee20e7b
cje206 <projec206@gmail.com>
2024-01-30 13:58:07 +0900

    javascript

--- week3/0111/exam3.js
+++ week3/0111/exam3.js
@@ -1,16 +1,29 @@
-$('td').on('click', function () {
-  $('#date').val($(this).children('.calDate').text());
-  $('#content').val($(this).children('.calContent').text());
-  let selectDate = $(this);
+// $('td').on('click', function () {
+//   $('#date').val($(this).children('.calDate').text());
+//   $('#content').val($(this).children('.calContent').text());
+// });
+
+// $('.write').on('click', function () {
+//   let writeDate = $('.calDate').eq(Number($('#date').val()) - 1);
+//   let writeContent = $('#content').val();
+
+//   console.log(writeDate.next().hasClass('calContent'));
+//   if (writeDate.next().hasClass('calContent')) {
+//     writeDate.next().remove();
+//   }
+//   writeDate.after('<span class="calContent">' + writeContent + '</span>');
+// });
+
+$('.calDate').on('click', function () {
+  $('#date').val($(this).text());
+
+  target = this;
 });
 
-$('.write').on('click', function () {
-  let writeDate = $('.calDate').eq(Number($('#date').val()) - 1);
-  let writeContent = $('#content').val();
+function writeSchedule() {
+  let content = $('#content').val();
 
-  console.log(writeDate.next().hasClass('calContent'));
-  if (writeDate.next().hasClass('calContent')) {
-    writeDate.next().remove();
-  }
-  writeDate.after('<span class="calContent">' + writeContent + '</span>');
-});
+  $(target).parent().append(`<span>${content}</span>`);
+  $('#content').val('');
+  $('#date').val('');
+}
